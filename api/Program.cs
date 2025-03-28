3a9c41a1f5051e11e7a3e7f47d8240a1d2fd018b
markWangsgard <mark.wangsgard@students.snow.edu>
2025-03-27 19:30:51 -0600

    API classes recipe and ingredient made. Enum Difficulty Start get request

--- api/Program.cs
+++ api/Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.VisualBasic;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+List<Recipe> recipeDatabase = new();
+
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/recipes", () => {
+    return recipeDatabase;
+});
+
 app.Run();
+
+
+public record Recipe(int Id, string Title, string Creator, string PhotoURL, int ServingSize, RecipeDifficulty Difficulty, List<Ingredient> Ingredients, List<string> Directions);
+public enum RecipeDifficulty {Easy, Standard, Medium, Intermediate, Hard}
+public record Ingredient (string Name, float Quantity, string Unit);
