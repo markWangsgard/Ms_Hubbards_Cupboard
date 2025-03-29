50bb5215a179b33abb9c51f1665e3463933d94e8
markWangsgard <mark.wangsgard@students.snow.edu>
2025-03-29 16:42:30 -0600

    start recipe template, fixed horizontal scroll on home page

--- client/JavaScript/service.js
+++ client/JavaScript/service.js
@@ -4,29 +4,4 @@ export const getAllRecipes = async () => {
   const response = await fetch(`${url}/recipes`);
   const objects = await response.json();
   return objects;
-  objects.Map((recipe) => {
-    let DifficultyRating;
-    switch (recipe.difficulty) {
-      case 1:
-        DifficultyRating = "Easy";
-        break;
-      case 2:
-        DifficultyRating = "Standard";
-        break;
-      case 3:
-        DifficultyRating = "Medium";
-        break;
-      case 4:
-        DifficultyRating = "Intermediate";
-        break;
-      case 5:
-        DifficultyRating = "Hard";
-        break;
-    }
-    const newRecipe = {
-        ...recipe,
-        difficultyRating: DifficultyRating
-    }
-    return newRecipe;
-  });
 };
