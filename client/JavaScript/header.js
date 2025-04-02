957f36637e1e20b58e94dcd806516de9b80deb3e
markWangsgard <mark.wangsgard@students.snow.edu>
2025-04-02 01:11:47 -0600

    Top nav bar added

--- client/JavaScript/header.js
+++ client/JavaScript/header.js
@@ -1,8 +1,26 @@
 const headerElement = document.getElementById("header");
+const logoAndTitleContainerElement = document.createElement("div");
 const logoElement = document.createElement("img");
 logoElement.src = "/images/logo.png";
 logoElement.alt = "Ms. Hubbard's Cupboard";
 const titleElement = document.createElement("h1");
 titleElement.textContent = "Ms Hubbard's Cupboard";
-headerElement.appendChild(logoElement);
-headerElement.appendChild(titleElement);
+const navElement = document.createElement("nav");
+navElement.classList = "mobile-hidden"
+const link1Element = document.createElement("a");
+link1Element.textContent = "Home";
+link1Element.href = "http://127.0.0.1:5500/html/index.html";
+const link2Element = document.createElement("a");
+link2Element.textContent = "Pantry";
+link2Element.href = "http://127.0.0.1:5500/html/pantry.html";
+const link3Element = document.createElement("a");
+link3Element.textContent = "Add a Recipe";
+link3Element.href = "http://127.0.0.1:5500/html/addRecipe.html"
+
+navElement.appendChild(link1Element);
+navElement.appendChild(link2Element);
+navElement.appendChild(link3Element);
+logoAndTitleContainerElement.appendChild(logoElement);
+logoAndTitleContainerElement.appendChild(titleElement);
+headerElement.appendChild(logoAndTitleContainerElement);
+headerElement.appendChild(navElement);
