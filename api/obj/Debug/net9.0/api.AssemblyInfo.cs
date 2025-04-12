ad86bca519ce9529e5ac25f47a34524b6ae65fbc
markWangsgard <mark.wangsgard@students.snow.edu>
2025-04-11 22:41:29 -0600

    attempt to have a relative path for links

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6ee7483bc09114d1e30d923050e01ebe73ed707")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84810a8e1a352dc531edb7713ef9b5d2438d3c65")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
