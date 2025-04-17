99b78556d22328605ae6c440b32206a21fd4ccac
Mark Wangsgard <mark.wangsgard@students.snow.edu>
2025-04-17 10:44:11 -0600

    Lab machine api

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e1674261afffcbaa4c88aabee99faca02070361")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db5b39d95b8f207e9406c62704931d72db9ae966")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
