83d9a131f0a917269f52f7424adb452a2a0e80e6
markWangsgard <mark.wangsgard@students.snow.edu>
2025-04-29 17:36:52 -0600

    Updated Font

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f51f8322fb167d87bf64a4742b6381a8216cdf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6774d8aa6dd9afbca4cb1c07d38d611b7dacaa40")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
